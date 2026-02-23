24dab33b85b338d24b0ae4889254d1b63c781794
Heng-I Chu <chu244@eceprog2.ecn.purdue.edu>
2026-02-23 04:30:42 -0500

    exercise 5 6

--- kern/trap.c
+++ kern/trap.c
@@ -262,6 +262,9 @@ page_fault_handler(struct Trapframe *tf)
 	// Handle kernel-mode page faults.
 
 	// LAB 3: Your code here.
+	if ((tf->tf_cs & 3) == 0)
+		panic("page_fault_handler: kernel-mode page fault at va %08x, eip %08x",
+		      fault_va, tf->tf_eip);
 
 	// We've already handled kernel-mode exceptions, so if we get here,
 	// the page fault happened in user mode.
